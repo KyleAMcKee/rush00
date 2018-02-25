b29c4ee3444b60da6a7bc7ffb7e3fd6281227099
Yan jun Zhu <yazhu@e1z2r9p39.42.us.org>
2018-02-25 03:36:29 -0800

    displays products with option to delete with button

--- manageinv.php
+++ manageinv.php
@@ -12,8 +12,10 @@
         <h2>Manage items</h2>
         <form class="signup-form" method="POST" action="addproduct.php"> 
             <input type="text" name="name" placeholder="Product Name">
-            <input type="text" name="price" placeholder="Price">
+            <input type="number" min='0' step='0.01' name="price" placeholder="Price">
+            <input type="number" min='0' name="stock" placeholder="Quantity">
             <input type="text" name="desc" placeholder="Description">
+            <input type="url" name="image" placeholder="Image URL">
             <button type="submit" name="submit" value="add">Add</button>
             <button type="submit" name="submit" value="delete">Delete</button>
             <button type="submit" name="submit" value="modify">Modify</button>
@@ -26,7 +28,8 @@
             echo '<ul class="products">';
             foreach ($items as $key => $value)
             {
-                echo '<li><img src="42.png"><br /><p>'.$value["name"].'</p><br />';
+                echo '<li><img src="'.$value["image"].'"><br /><p>'.$value["name"].'</p><br />';
+                echo '<p>$'.$value["price"].'</p><br />';
                 echo '<form method="POST" action="addproduct.php">';
                 echo '<input type="hidden" name="name" value="'.$value["name"].'">';
                 echo '<button type="submit" name="submit" value="delete">Delete</button></form>';
