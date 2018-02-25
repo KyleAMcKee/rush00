2744c10d01100ecb86d53323e05dae186c2659cb
Yan jun Zhu <yazhu@e1z2r9p39.42.us.org>
2018-02-25 03:39:22 -0800

    commented out some unused code

--- productlist.php
+++ productlist.php
@@ -8,20 +8,20 @@ if (isset($_GET['action']) && !isset($_SESSION['id']))
         header("Location: index.php?item_added");
         exit();
     }
-    if ($_GET['action'] == "delete")
-    {
-        if ($_SESSION['cart'][$_GET['id']] > 0)
-        {
-            $_SESSION['cart'][$_GET['id']]--;
-            header("Location: index.php?item_removed");
-            exit();
-        }
-    }
-    else
-    {
-        header("Location: index.php?item_not_in_cart");
-        exit();
-    }
+    // if ($_GET['action'] == "delete")
+    // {
+    //     if ($_SESSION['cart'][$_GET['id']] > 0)
+    //     {
+    //         $_SESSION['cart'][$_GET['id']]--;
+    //         header("Location: index.php?item_removed");
+    //         exit();
+    //     }
+    // }
+    // else
+    // {
+    //     header("Location: index.php?item_not_in_cart");
+    //     exit();
+    // }
 }
 //Item quantity modified and user IS logged in
 else if (isset($_GET['action']) && isset($_SESSION['id']))
@@ -70,14 +70,14 @@ else if (isset($_GET['action']) && isset($_SESSION['id']))
 }
 ?>
 
-
 <?php
     $items = unserialize(file_get_contents('../data/products'));
     echo '<h1>Product List</h1>';
     echo '<ul class="products">';
     foreach ($items as $key => $value)
     {
-        echo '<li><img src="42.png"><br /><p>'.$value["name"].'</p><br />';
+        echo '<li><img src="'.$value["image"].'"><br /><p>'.$value["name"].'</p><br />';
+        echo '<p>$'.$value["price"].'</p><br />';
         echo '<a href="index.php?action=add&id='.$key.'">Add to Cart</a>';
     }
     echo '</ul>'; 
