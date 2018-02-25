c1e8a050bbe96a051444d35061e21a9d90ae614f
Yan jun Zhu <yazhu@e1z2r9p39.42.us.org>
2018-02-25 03:41:01 -0800

    script for deleting users from admin page

---
+++ deleteusers.php
@@ -0,0 +1,29 @@
+<?php
+	//DELETE
+	if (isset($_POST['submit']) && $_POST['submit'] == 'delete')
+	{
+		$users = unserialize(file_get_contents('../data/passwd'));
+		if (empty($users[$_POST['uid']]))
+		{
+			header("Location: manageusers.php?manageusers=empty");
+			exit();
+
+		}
+		else if ($users[$_POST['uid']]['priv'] === false)
+			unset($users[$_POST['uid']]);
+		else
+		{
+			header("Location: manageusers.php?manageusers=adminnotdeleted");
+			exit();
+		}
+        $users = serialize($users);
+        file_put_contents('../data/passwd', $users);
+        header("Location: manageusers.php?manageusers=userdeleted>");
+        exit();
+	}
+    else
+    {
+    	header("Location: manageusers.php");
+    	exit();
+    }
+?>
